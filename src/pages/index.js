e8b46e785f5231cab8a0bbac4470349910c57bd4
sangram <sangramvbarge@gmail.com>
2020-04-18 16:22:52 +0530

    updated home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World</h1>
+    <p>Work in progress. Version 1.0 will be created soon</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
