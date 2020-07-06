08e43222424335ac7fd92ad2a4275b960f4f37ed
sangram <sangramvbarge@gmail.com>
2020-07-06 22:42:24 +0530

    updated footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -59,7 +59,7 @@ const Layout = ({ location, title, children }) => {
       <header>{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Made in India Made with ❤️
+        © {new Date().getFullYear()} Made with ❤️ in India.
         {` `}
         <a href="https://twitter.com/sangramtechs">Twitter</a>
       </footer>
